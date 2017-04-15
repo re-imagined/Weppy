71b59659cfe2b4712bbf6226a8c5ef47bb8c5f57
seniorcandy <seniorgiftcandy@gmail.com>
2017-04-15 16:53:16 +0800

    syntax error fix
    Signed-off-by: seniorcandy <seniorgiftcandy@gmail.com>

--- www/app.py
+++ www/app.py
@@ -39,73 +39,74 @@ def init_jinja2(app, **kw):
 
 @asyncio.coroutine
 def logger_factory(app, handler):
-    async def logger(request):
+    @asyncio.coroutine
+    def logger(request):
         logging.info('Request: %s %s' % (request.method, request.path))
-        # await asyncio.sleep(0.3)
-        return (await handler(request))
+        return (yield from handler(request))
     return logger
 
 
 @asyncio.coroutine
 def data_factory(app, handler):
-    async def parse_data(request):
+    @asyncio.coroutine
+    def parse_data(request):
         if request.method == 'POST':
             if request.content_type.startswith('application/json'):
-                request.__data__ = await request.json()
+                request.__data__ = yield from request.json()
                 logging.info('request json: %s' % str(request.__data__))
             elif request.content_type.startswith(
                     'application/x-www-form-urlencoded'):
-                request.__data__ = await request.post()
+                request.__data__ = yield from request.post()
                 logging.info('request form: %s' % str(request.__data__))
-        return (await handler(request))
+        return (yield from handler(request))
     return parse_data
 
 
 @asyncio.coroutine
 def response_factory(app, handler):
-    async def response(request):
+    @asyncio.coroutine
+    def response(request):
         logging.info('Response handler...')
-        r = await handler(request)
+        r = yield from handler(request)
         if isinstance(r, web.StreamResponse):
             return r
         if isinstance(r, bytes):
-            resp = web.Response(body=r)
-            resp.content_type = 'application/octet-stream'
-            return resp
+            response = web.Response(body=r)
+            response.content_type = 'application/octet-stream'
+            return response
         if isinstance(r, str):
             if r.startswith('redirect:'):
                 return web.HTTPFound(r[9:])
-            resp = web.Response(body=r.encode('utf-8'))
-            resp.content_type = 'text/html;charset=utf-8'
-            return resp
+            response = web.Response(body=r.encode('utf-8'))
+            response.content_type = 'text/html;charset=utf-8'
+            return response
         if isinstance(r, dict):
             template = r.get('__template__')
             if template is None:
-                resp = web.Response(
+                response = web.Response(
                     body=json.dumps(
                         r, ensure_ascii=False, default=lambda d: d.__dict__
                     ).encode('utf-8'))
-                resp.content_type = 'application/json;charset=utf-8'
-                return resp
+                response.content_type = 'application/json;charset=utf-8'
+                return response
             else:
-                resp = web.Response(
+                response = web.Response(
                     body=app['__templating__']
                     .get_template(template)
                     .render(**r)
                     .encode('utf-8')
                 )
-                resp.content_type = 'text/html;charset=utf-8'
-                return resp
+                response.content_type = 'text/html;charset=utf-8'
+                return response
         if isinstance(r, int) and r >= 100 and r < 600:
             return web.Response(r)
         if isinstance(r, tuple) and len(r) == 2:
             t, m = r
             if isinstance(t, int) and t >= 100 and t < 600:
                 return web.Response(t, str(m))
-        # default:
-        resp = web.Response(body=str(r).encode('utf-8'))
-        resp.content_type = 'text/plain;charset=utf-8'
-        return resp
+        response = web.Response(body=str(r).encode('utf-8'))
+        response.content_type = 'text/plain;charset=utf-8'
+        return response
     return response
 
 
